43b28e48c34d521bc7ca56f81516ae73693daedd
Alin <alinjobo@gmail.com>
2022-05-28 11:48:54 +0530

    made basic UI

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a,b){
+    return a+b;
+};
+function sub(a,b){
+    return a-b;
+};
+function mul(a,b){
+    return a*b;
+
+};
+function div(a,b){
+    if(b=0)
+    return "Cant divide by zero"
+    return a/b;
+};
+function operate(operator,a,b){
+    switch (operator){
+        case '+': return add(a,b);
+        break;
+        case '-': return sub(a,b);
+        break;
+        case '*': return mul(a,b);
+        break;
+        case '/' :return div(a,b);
+        break;
+    };
+};
+inp=document.querySelector('.box');
