1eb11c82869af45db01910189122de3af5698a9d
Alin Jobo <alinjob@gmail.com>
2022-06-02 13:19:04 +0530

    multiple numbers

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
+let temp;
+
 function add(a,b){
-    return a+b;
+    
+   temp= parseInt(a)+parseInt(b);
+    return temp.toString();
+
 };
 function sub(a,b){
-    return a-b;
+    temp= parseInt(a)-parseInt(b);
+    return temp.toString();
 };
 function mul(a,b){
-    return a*b;
+    temp= parseInt(a)*parseInt(b);
+    return temp.toString();
 
 };
 function div(a,b){
-    console.log(a);
-    console.log(b);
-    console.log(a/b);
+    
     if(b==0)
-    return "Cant divide by zero"
-    return a/b;
+    return "Cant divide by zero";
+    temp= parseInt(a)/parseInt(b);
+    return temp.toString();
 };
 function operate(operator,a,b){
+    if(b=='')
+    return a;
     switch (operator){
         case '+' : return add(a,b);
         break;
@@ -24,47 +32,78 @@ function operate(operator,a,b){
         break;
         case '*' : return mul(a,b);
         break;
-        case '/' :return div(a,b);
+        case '/' :return div(a,b);  
         break;
     };
 };
+function dispnum(){
+    field.textContent+=this.textContent;
+    fieldvalue=field.textContent;   
+
+    
+};
+function operation(){
+    value2+=this.textContent;
+    field.textContent+=this.textContent;
+    console.log('value2',value2);
+    console.log('v1',value1);
+      
+    console.log('v1',value1);   
+   
+    console.log(fieldvalue);
+
+};
 inp=document.querySelector('.field');
-let fieldvalue;
-const field=document.querySelector('.field')
+let fieldvalue='';
+const field=document.querySelector('.field');
 const numb=document.querySelectorAll('.num');
 numb.forEach(numbutton=> {
-    numbutton.addEventListener('click',()=>{
-        field.textContent+=numbutton.textContent;
-        fieldvalue=field.textContent;
-
-        
-    })
+    numbutton.addEventListener('click',dispnum);
+    
 });
 const clear=document.querySelector('.clear');
 clear.addEventListener('click',()=>{
-    field.textContent='';
+    location.reload();
 });
- console.log(numb.innerHTML);
- let value1,op,value2;
+ 
+ let value1,op;
+ let value2='';
 const operator=document.querySelectorAll('.operator');
 operator.forEach(opbutton=>{
     opbutton.addEventListener('click',()=>{
-        
-        value1=field.textContent;
+       if(!(isNaN(field.textContent.slice(-1)))){ 
+        op=opbutton.textContent; 
+        fieldvalue=operate(op,fieldvalue,value2);  
+        value2='';
         field.textContent+=opbutton.textContent;
-        op=opbutton.textContent;
+        
+        
+        numb.forEach(numbutton=> {
+            numbutton.removeEventListener('click',dispnum);
+        });
+             
+        
+        console.log('text is', field.textContent);
+        console.log('index of op',field.textContent.indexOf(op)+1);
 
+        numb.forEach(numbutton=> {
+            numbutton.addEventListener('click',operation);
+        });
+        
+        
+        }
     });
 });
-
+//
 const equal=document.querySelector('.equals');
 equal.addEventListener('click',()=>{
-    value2=field.textContent.slice(field.textContent.indexOf(op)+1);
-    console.log(value1);
-    console.log(value2);
-    console.log(op);
-    field.textContent=operate(op,value1,value2);
+    fieldvalue=operate(op,fieldvalue,value2); 
+    field.textContent=fieldvalue;
+    fieldvalue='';
+    numb.forEach(numbutton=> {
+        numbutton.removeEventListener('click',operation);
+        numbutton.addEventListener('click',dispnum);
+    });
+
    // field.textContent=operate(op,parseInt(value1),parseInt(value2));
 });
-
-
